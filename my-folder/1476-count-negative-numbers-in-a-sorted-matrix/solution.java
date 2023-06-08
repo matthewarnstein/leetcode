ff7f383c67009947dc81489da2473823172d51bf
Matthew Arnstein <600622+matthewarnstein@users.noreply.github.com>
2023-06-08 20:27:52 +0000

    [LeetCode Sync] Runtime - 1 ms (56.21%), Memory - 43.7 MB (99.21%)

---
+++ my-folder/1476-count-negative-numbers-in-a-sorted-matrix/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int neg = 0;
+        for (int[] row : grid) {
+            for (int num : row) {
+                if (num < 0)
+                    neg++;
+            }
+        }
+        return neg;
+    }
+}
