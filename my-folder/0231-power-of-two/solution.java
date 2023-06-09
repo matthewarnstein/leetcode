6a6995d17d8bfe71bf5a8151437db9588914b9cf
Matthew Arnstein <600622+matthewarnstein@users.noreply.github.com>
2023-06-09 13:12:08 +0000

    [LeetCode Sync] Runtime - 1 ms (44.53%), Memory - 40 MB (98.85%)

---
+++ my-folder/0231-power-of-two/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 1)
+            return true;
+        else if (n % 2 == 1 || n == 0) {
+            return false;
+        } else {
+            return isPowerOfTwo(n/2);
+        }
+    }
+
+}
