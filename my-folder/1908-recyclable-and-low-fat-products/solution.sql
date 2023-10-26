c5ada74322698469f015e0ee1a9add2f6495210a
Matthew Arnstein <600622+matthewarnstein@users.noreply.github.com>
2023-10-26 02:43:51 +0000

    [LeetCode Sync] Runtime - 526 ms (59.28%), Memory - 0B (100.00%)

--- my-folder/1908-recyclable-and-low-fat-products/solution.sql
+++ my-folder/1908-recyclable-and-low-fat-products/solution.sql
@@ -1,2 +1,2 @@
-select  product_id from Products where low_fats = 'Y' and recyclable = 'Y';
-
+-- Write your PostgreSQL query statement below
+select product_id from Products where low_fats = 'Y' and recyclable = 'Y';
