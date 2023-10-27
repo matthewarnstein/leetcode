82c8483fd5431613f29a1b6a71f798e89d13efaf
Matthew Arnstein <600622+matthewarnstein@users.noreply.github.com>
2023-10-27 12:35:59 +0000

    [LeetCode Sync] Runtime - 526 ms (65.39%), Memory - 0B (100.00%)

---
+++ my-folder/0175-combine-two-tables/solution.sql
@@ -0,0 +1,2 @@
+select Person.firstName as firstName, Person.lastName as lastName, Address.city as city, Address.state as state from Person left join Address on Person.personId = Address.personId;
+
