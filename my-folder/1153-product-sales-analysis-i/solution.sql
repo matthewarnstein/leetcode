32f041afab54ecae51ee885f9ee07a825cddaa5d
Matthew Arnstein <600622+matthewarnstein@users.noreply.github.com>
2024-02-27 19:44:32 +0000

    [LeetCode Sync] Runtime - 2905 ms (16.86%), Memory - 0B (100.00%)

---
+++ my-folder/1153-product-sales-analysis-i/solution.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT 
+    Product.product_name, 
+    Sales.year, 
+    Sales.price
+FROM 
+    Sales
+JOIN 
+    Product ON Sales.product_id = Product.product_id
+ORDER BY 
+    Sales.sale_id;
