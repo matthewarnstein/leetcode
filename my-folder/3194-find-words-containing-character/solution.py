86c2d3f549e1179949fc6fd0f86f504a33935324
Matthew Arnstein <600622+matthewarnstein@users.noreply.github.com>
2024-04-10 02:12:26 +0000

    [LeetCode Sync] Runtime - 63 ms (36.64%), Memory - 16.5 MB (78.35%)

---
+++ my-folder/3194-find-words-containing-character/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        res = []
+        for index, word in enumerate(words):
+            if x in word:
+                res.append(index)
+        
+        return res;
