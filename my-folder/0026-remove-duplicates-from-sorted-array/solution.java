1db3d8c8c22d8a397a31f06b607b21ca4990dfcb
Matthew Arnstein <600622+matthewarnstein@users.noreply.github.com>
2024-04-15 18:45:51 +0000

    [LeetCode Sync] Runtime - 1 ms (73.47%), Memory - 44.6 MB (51.65%)

--- my-folder/0026-remove-duplicates-from-sorted-array/solution.java
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int curr = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[curr] != nums[i]) {
-                curr++;
-                nums[curr] = nums[i];
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
             }
         }
-        curr++;
-
-        return curr; 
+        return i + 1;
     }
 }
